fc494cff4890a4da762752965b1b9525abdd3c2a
kpdno <ex.madara0@gmail.com>
2023-01-10 23:54:59 -0500

    update

---
+++ src/main/java/utalities/ExcelUtalities.java
@@ -0,0 +1,18 @@
+package utalities;
+
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+public class ExcelUtalities {
+
+	public static XSSFWorkbook openExcel (String location) {
+			return null;
+	}
+	public static XSSFSheet getSheetByName(String sheetName, String workbookName) {
+			return null;
+			}
+	public static Object[][] getDatafromtable( String tableName, String sheetName, String workBook){
+			return null;
+	}
+}
+	
